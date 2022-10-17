18af20fa767f3fdc08c88b4a9d02d2d73a35f399
7HHY <yallluu@gmail.com>
2022-10-17 18:22:49 +0900

    Lv1

---
+++ kakao_test/2022_INTERNSHIP/Lv1_성격유형검사하기/Solution.py
@@ -0,0 +1,30 @@
+def solution(survey, choices):
+    scores = {"A":0, "N":0, "C":0, "F":0, "M":0, "J":0, "R":0, "T":0}
+    for i, choice in enumerate(choices):
+        if choice - 4 > 0:
+            scores[survey[i][1]] += choice - 4
+        elif choice - 4 < 0:
+            scores[survey[i][0]] += 4 - choice
+
+    type = ""
+    if scores["R"] >= scores["T"]:
+        type += "R"
+    else:
+        type += "T"
+
+    if scores["C"] >= scores["F"]:
+        type += "C"
+    else:
+        type += "F"
+
+    if scores["J"] >= scores["M"]:
+        type += "J"
+    else:
+        type += "M"
+
+    if scores["A"] >= scores["N"]:
+        type += "A"
+    else:
+        type += "N"
+
+    return type
