b6acf7555301fdec0c14a484ee892fcfbc5e6d0c
JHHY <101182589+7HHY@users.noreply.github.com>
2022-10-24 11:54:59 +0900

    Delete Lv1 directory

--- Lv1/짝수와홀수/Solution.py
+++
@@ -1,9 +0,0 @@
-def solution(num):
-    if num % 2 == 0:
-        answer = "Even"
-    elif num == 0:
-        answer = "Even"
-    else :
-        answer = "Odd"
-    
-    return answer
--- Lv1/평균구하기/Solution.py
+++
@@ -1,6 +0,0 @@
-def solution(arr):
-    a = 0
-    for i in arr:
-        a += i
-    answer = a / len(arr)
-    return answer
